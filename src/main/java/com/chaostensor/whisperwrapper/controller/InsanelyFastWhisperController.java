4e7e5575c613d6346be2e7b88bb0e25efaa971c2
Rusty Lopez <rustylopez@gmail.com>
2026-05-02 20:56:58 -0600

    compile fix

--- src/main/java/com/chaostensor/whisperwrapper/controller/InsanelyFastWhisperController.java
+++ src/main/java/com/chaostensor/whisperwrapper/controller/InsanelyFastWhisperController.java
@@ -19,6 +19,7 @@ import reactor.core.scheduler.Schedulers;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
