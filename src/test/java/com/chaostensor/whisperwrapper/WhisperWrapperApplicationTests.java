b97d6540b8f3a825c98396d5a90b28eb42d3700b
Rusty Lopez <rustylopez@gmail.com>
2026-05-03 01:14:54 -0600

    tests

--- src/test/java/com/chaostensor/whisperwrapper/WhisperWrapperApplicationTests.java
+++ src/test/java/com/chaostensor/whisperwrapper/WhisperWrapperApplicationTests.java
@@ -2,8 +2,10 @@ package com.chaostensor.whisperwrapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yaml" })
 class WhisperWrapperApplicationTests {
 
 	@Test
--- src/test/java/com/chaostensor/whisperwrapper/controller/WhisperControllerTest.java
+++ src/test/java/com/chaostensor/whisperwrapper/controller/WhisperControllerTest.java
@@ -6,6 +6,7 @@ import com.chaostensor.whisperwrapper.repository.WhisperJobRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,6 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yaml" })
 class WhisperControllerTest {
 
     @Autowired
