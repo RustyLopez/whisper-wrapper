3e9caaa3a06bc04de8b341c75d4c39690e19bb52
Rusty Lopez <rustylopez@gmail.com>
2026-05-03 01:26:45 -0600

    tests

--- src/test/java/com/chaostensor/whisperwrapper/WhisperWrapperApplicationTests.java
+++ src/test/java/com/chaostensor/whisperwrapper/WhisperWrapperApplicationTests.java
@@ -4,9 +4,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+@Testcontainers
 @SpringBootTest
 @TestPropertySource(properties = { "spring.config.location=classpath:application-test.yaml" })
-class WhisperWrapperApplicationTests {
+public class WhisperWrapperApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgresWithVector = new PostgreSQLContainer<>("pgvector/pgvector:pg18")
+			.withDatabaseName("testdb")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void registerProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.r2dbc.url", postgresWithVector::getR2dbcUrl);
+		registry.add("spring.r2dbc.username", postgresWithVector::getUsername);
+		registry.add("spring.r2dbc.password", postgresWithVector::getPassword);
+
+		registry.add("spring.datasource.url", postgresWithVector::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresWithVector::getUsername);
+		registry.add("spring.datasource.password", postgresWithVector::getPassword);
+		registry.add("spring.datasource.driver-class-name", "rg.postgresql.Driver");
+
+	}
+
+	@Autowired
+	private DatabaseClient databaseClient;
+
 
 	@Test
 	void contextLoads() {
