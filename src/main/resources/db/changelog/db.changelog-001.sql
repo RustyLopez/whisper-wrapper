d656074b3a1438a54f3e2a02d0bb5631885555e6
Rusty Lopez <rustylopez@gmail.com>
2026-05-03 03:49:45 -0600

    yaml liquibase

--- src/main/resources/db/changelog/db.changelog-001.sql
+++ src/main/resources/db/changelog/db.changelog-001.sql
@@ -4,12 +4,9 @@
 CREATE TABLE whisper_jobs (
     id UUID PRIMARY KEY NOT NULL,
     hash VARCHAR(64),
-    status VARCHAR(20) NOT NULL,
+    status jsonb NOT NULL,
     transcript_text TEXT,
     video_path VARCHAR(500) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
-
---changeset opencode:update-status-column-to-json
-ALTER TABLE whisper_jobs ALTER COLUMN status TYPE json;
